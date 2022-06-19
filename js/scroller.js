f5a633a07d6135f96aaf62d46e996009bb54db63
nem0z <maxime.bernard.dev@gmail.com>
2022-06-19 18:26:52 +0200

    resize pp when scroll

--- js/scroller.js
+++ js/scroller.js
@@ -1,5 +1,11 @@
 (function() {
 
-    
+    document.addEventListener('scroll', e => {
+        if(window.scrollY > window.innerHeight) {
+            document.querySelector('#profil-picture').classList.add('small');
+        } else {
+            document.querySelector('#profil-picture').classList.remove('small');
+        }
+    });
 
 })();
