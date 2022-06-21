0093481ecc13c7917c9fbd50b00d99a0fc0afb9f
nem0z <maxime.bernard.dev@gmail.com>
2022-06-22 01:58:29 +0200

    update competence finally

--- js/competence.js
+++ js/competence.js
@@ -2,7 +2,7 @@
     let competences_li = document.querySelectorAll('.list-competence > li');
 
     for(let competence_li of competences_li) {
-        competence_li.id = competence_li.innerText;
+        competence_li.id = competence_li.innerText.replaceAll(' / ', '-').replaceAll('+', 'p').replaceAll('.', '-').replaceAll(' ', '-');
     }
 
     let colors = {1: 'red', 2: 'orange', 3: 'green'};
@@ -11,6 +11,7 @@
     .then(res => res.json())
     .then(competences => {
         for(let competence of competences) {
+            console.log(competence);
             document.querySelector(`#${competence.label}`).classList.add(`border-${colors[competence.level]}`);
         }
     });
